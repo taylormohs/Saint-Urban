ee43236b2eeeef608edc79d1a2d14ccc92853a27
taylormohs <taylormohhs@gmail.com>
2025-01-26 18:28:10 -0700

    Day One complete

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const titles = document.querySelectorAll('.title');
+const descriptions = document.querySelectorAll('.description');
 
+let isDescriptionVisible = Array(titles.length).fill(false);
+titles.forEach((title, index) => {
+    title.addEventListener('click', () => {
+        isDescriptionVisible[index] = !isDescriptionVisible[index];
+        descriptions[index].style.display = isDescriptionVisible[index] ? 'block' : 'none';
+    });
+});
