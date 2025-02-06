a1151736dd28321aec8033914675dd0ce0ecd2c9
taylormohs <taylormohhs@gmail.com>
2025-02-06 16:30:11 -0700

    Can type wine answers

--- script.js
+++ script.js
@@ -6,5 +6,24 @@ titles.forEach((title, index) => {
     title.addEventListener('click', () => {
         isDescriptionVisible[index] = !isDescriptionVisible[index];
         descriptions[index].style.display = isDescriptionVisible[index] ? 'block' : 'none';
+    }); 
+});
+
+const checkBtns = document.querySelectorAll('.check-btn');
+const answers = document.querySelectorAll('.answer');
+let userInput = Array.from(document.querySelectorAll('.user-input'));
+
+let isAnswerVisible = Array(checkBtns.length).fill(false);
+
+checkBtns.forEach((checkBtn, index) => {
+    checkBtn.addEventListener('click', () => {
+        isAnswerVisible[index] = !isAnswerVisible[index];
+        
+        if (userInput[index].value === answers[index].textContent) {
+            answers[index].style.display = 'block';
+            answers[index].textContent = `Correct!`;
+        } else {
+            answers[index].style.display = isAnswerVisible[index] ? 'block' : 'none';
+        }
     });
 });
